3422b9351fb3972d3349c9c32c67b4a28c7e5bfd
Anatol <anatol.karlinski@icloud.com>
2018-05-31 14:15:56 +0200

    First commmit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Andrzej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
